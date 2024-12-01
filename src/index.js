855b5d94044dffcdd54a9dc2400e915d0d40089d
NooMaaBee <latteliten@hotmail.com>
2024-12-01 17:17:09 +0100

    trying to make the forcast for the next 5 days and still have the current day in weather-app-data

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ function getForecast(city) {
 function displayForecast(response) {
   let forecastHtml = "";
   response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
+    if (index > 0 && index <= 5) {
       forecastHtml =
         forecastHtml +
         ` <div class="weather-forecast-day">
